81ea1ebb19bdbb989827e28c1a7e41be7dfbcfa5
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-09-27 10:33:06 -0300

    produtores e consumidores

--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -6,14 +6,21 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
-    public static void main(String[] args) {
-        var producer = new KafkaProducer<String, String>(properties());
-        var value = "2342, 34344, 44342";
-        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
 
-        producer.send(record);
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        var producer = new KafkaProducer<String, String>(properties());
+        var value = "132123,67523,1234";
+        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
+        producer.send(record, (data, ex) -> {
+            if(ex != null) {
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println("sucesso enviando " + data.topic() + ":::partition " + data.partition() + "/ offset "  + data.offset() + "/ timestamp " + data.timestamp());
+        }).get();
     }
 
     private static Properties properties() {
